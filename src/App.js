736dbed37fd5357ea4ad34e158be916d50c2aedb
Schatten WORK-PC <schattenwork@example.com>
2022-04-08 12:17:07 +0300

    added axios to project, console log list users, delete test-task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  console.log('Test task is done');
+  
+  axios.get('https://swapi.dev/api/people')
+    .then((res) => console.log(res.data.results))
+    .catch((error) => console.error(error))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React!</h1>
     </div>
   );
 }
