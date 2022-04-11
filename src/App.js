cabe8c4287fb84ea11be2536b9c9781bd25b2cec
Schatten WORK-PC <schattenwork@example.com>
2022-04-11 14:47:40 +0300

    refact input compontent

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import MyInput from './components/UI/input/MyInput';
-import MyInputPassword from './components/UI/input/MyInputPassword';
 
 function App() {
-  
+
   axios.get('https://swapi.dev/api/people')
     .then((res) => console.log(res.data.results))
     .catch((error) => console.error(error))
@@ -13,12 +12,12 @@ function App() {
   return (
     <div className="App">
       <div>
-      <h1>Hello React!</h1>
-      <MyInput id="email" type="email" label="Email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$"/>
-      <MyInputPassword id="pass" type="password" label="Password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"/>
-      <MyInputPassword id="confpass" type="password" label="Confirm password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"/>
-      <MyInput id="fname" type="text" label="First name" />
-      <MyInput id="lname" type="text" label="Last name" />
+        <h1>Hello React!</h1>
+        <MyInput id="email" type="email" label="Email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$" />
+        <MyInput id="pass" type="password" label="Password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" />
+        <MyInput id="confpass" type="password" label="Confirm password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" />
+        <MyInput id="fname" type="text" label="First name" />
+        <MyInput id="lname" type="text" label="Last name" />
       </div>
     </div>
   );
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './MyInput.module.css';
 
 const MyInput = (props) => {
-    return (
-        <div className={classes.divInput}>
-            <div>
-            <input id={props.id} className={classes.myInput} type={props.type} placeholder=" " pattern={props.pattern}/>
-            <label htmlFor={props.id} className={classes.labelInput}>{props.label}</label>
-            </div>
-        </div>
-    );
+
+	const [showPassword, setShowPassword] = useState(false);
+
+	const handleShowPassword = () => {
+		setShowPassword(showPassword ? false : true);
+	}
+
+	return (
+		<div className={classes.input__container}>
+			<input id={props.id} className={classes.input} type={props.type === 'password' && showPassword ? "text" : "password"} placeholder=" " pattern={props.pattern} />
+			<label htmlFor={props.id} className={classes.input__label}>{props.label}</label>
+			{props.type === 'password' &&
+				<div className={classes.button__container}>
+					<button className={showPassword ? classes.password__button + ' ' + classes.password__button_active : classes.password__button} onClick={handleShowPassword}></button>
+				</div>}
+		</div>
+	);
 };
 
 export default MyInput;
