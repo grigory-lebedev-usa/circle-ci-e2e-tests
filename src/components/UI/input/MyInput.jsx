4e2a8e1d1b2cc6856ea2d60caa9400a8d75a0f21
Schatten WORK-PC <schattenwork@example.com>
2022-04-11 16:01:42 +0300

    refact className input

--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -8,18 +8,16 @@ const MyInput = (props) => {
 	const handleShowPassword = () => {
 		setShowPassword(showPassword ? false : true);
 	}
-
-  const classA = classes.password__button;
-  const classB = classA + ' ' + classes.password__button_active;
-  const typeIf = props.type !== 'password' ? 'text' : props.type === 'password' && showPassword ? 'text' : 'password';
+  
+  const inputType = props.type !== 'password' ? 'text' : props.type === 'password' && showPassword ? 'text' : 'password';
 
 	return (
 		<div className={classes.input__container}>
-			<input id={props.id} className={classes.input} type={typeIf} placeholder=' ' pattern={props.pattern} />
+			<input id={props.id} className={classes.input} type={inputType} placeholder=' ' pattern={props.pattern} />
 			<label htmlFor={props.id} className={classes.input__label}>{props.label}</label>
 			  {props.type === 'password' &&
 				<div className={classes.button__container}>
-					<button className={showPassword ? classB : classA} onClick={handleShowPassword}></button>
+					<button className={showPassword ? `${classes.password__button} ${classes.password__button_active}` : classes.password__button} onClick={handleShowPassword}></button>
 				</div>}
 		</div>
 	);
