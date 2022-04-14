ba9a13dfbe8b7dd532d6266efae01c69f7751770
Schatten <schattenoff@example.com>
2022-04-14 14:51:46 +0300

    fix build notification [TAXIT-18]

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import FormDropDown from "./components/form/dropdown/FormDropDown";
 import FormCheckbox from "./components/form/checkbox/FormCheckbox";
 import Link from "./components/link/Link";
 import FormButton from "./components/form/button/FormButton";
-import Notification from "./components/notification/Notification";
 
 function App() {
   
--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import classes from "./Notification.module.css";
 import { notificationTypes } from "../../shared/enums";
 
