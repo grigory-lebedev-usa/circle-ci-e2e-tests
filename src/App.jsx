464b06188961cf16f61296a24467a9b7b406abac
Schatten WORK-PC <schattenwork@example.com>
2022-04-26 14:04:20 +0300

    page-wrapper done [TAXIT-33]

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './App.css';
+import Header from './components/Header/Header';
 import PageWrapper from './shared/components/PageWrapper/PageWrapper';
-// import uniqid from 'uniqid';
-
-// import FormInput from './shared/components/form-elements/FormInput/FormInput';
-// import FormCheckbox from './shared/components/form-elements/FormCheckbox/FormCheckbox';
-// import FormDropDown from './shared/components/form-elements/FormDropDown/FormDropDown';
-import FormButton from './shared/components/form-elements/FormButton/FormButton';
-// import Textarea from './shared/components/Textarea/Textarea';
-// import ProgressSpinner from './shared/components/ProgressSpinner/ProgressSpinner';
-// import Notifications from './shared/components/Notifications/Notifications';
-// import Modal from './shared/components/Modal/Modal';
-// import Link from './shared/components/Link/Link';
-// import Hint from './shared/components/Hint/Hint';
-// import DropDown from './shared/components/DropDown/DropDown';
-// import Button from './shared/components/Button/Button';
-// import { MAX_NOTIFICATION_NUMBER } from './shared/components/Notifications/notifications.constants';
-// import { buttonColors, buttonSizes } from './shared/components/Button/button.constants';
-// import { notificationTypes } from './shared/components/Notifications/components/Notification/notification.constants';
-// import { inputTypes } from './shared/components/form-elements/FormInput/form-input.constants';
 
 function App() {
-  // const [visibilitySpinner, setVisibilitySpinner] = useState(false);
-  // const [openedModal, setOpenedModal] = useState(false);
-  const [isLogin, setIsLogin] = useState(true);
-  // const [notifications, setNotifications] = useState([]);
-  // const notificationsRef = useRef([]);
-
-  // const openModal = () => {
-  //   setOpenedModal(true);
-  // };
-
-  // const closeModal = () => {
-  //   setOpenedModal(false);
-  // };
-
-  // const showNotification = (text, type) => {
-  //   if (notifications.length === MAX_NOTIFICATION_NUMBER) {
-  //     notificationsRef.current.shift();
-  //   }
-  //   notificationsRef.current.push({ text, type, id: uniqid() });
-  //   setNotifications([...notificationsRef.current]);
-  // };
-
-  // const deleteNotification = useCallback((id) => {
-  //   notificationsRef.current = notificationsRef.current.filter(
-  //     (notification) => notification.id !== id
-  //   );
-  //   setNotifications(notificationsRef.current);
-  // }, []);
-
-  // const showSpinner = () => {
-  //   setVisibilitySpinner(true);
-  //   setTimeout(() => {
-  //     setVisibilitySpinner(false);
-  //   }, 3000);
-  // };
-
-  const handleLogout = () => {
-    setIsLogin(false);
-  };
   return (
     <div>
-      {/* <Notifications notifications={notifications} onDelete={deleteNotification} />
-      <ProgressSpinner isOpened={visibilitySpinner} />
-       */}
-      <PageWrapper isLogin={isLogin} logout={handleLogout}>
-        <FormButton onClick={() => setIsLogin(true)}>Login</FormButton>
+      <PageWrapper>
+        <Header />
       </PageWrapper>
     </div>
   );
--- src/shared/components/PageWrapper/PageWrapper.jsx
+++ src/shared/components/PageWrapper/PageWrapper.jsx
@@ -2,52 +2,14 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import DropDown from '../DropDown/DropDown';
-
 import classes from './page-wrapper.module.css';
 
-function PageWrapper({ children, isLogin, logout }) {
-  return (
-    <div className={classes.wrapper}>
-      <div className={classes.dropdown__block}>
-        <DropDown
-          items={[
-            { id: 1, value: 'English' },
-            { id: 2, value: 'Russian' },
-            { id: 3, value: 'German' }
-          ]}
-        />
-      </div>
-      {isLogin && (
-        <div className={classes.logout__block}>
-          <button onClick={logout} className={classes.logout_btn} type="submit">
-            Log out
-          </button>
-        </div>
-      )}
-      {isLogin && (
-        <div className={classes.header}>
-          <div className={classes.container}>
-            <div className={classes.header__content}>
-              <h1 className={classes.header__title}>GeneralSoft Taxi</h1>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {children}
-    </div>
-  );
+function PageWrapper({ children }) {
+  return <div className={classes.wrapper}>{children}</div>;
 }
 
 PageWrapper.propTypes = {
-  children: PropTypes.node.isRequired,
-  isLogin: PropTypes.bool,
-  logout: PropTypes.func.isRequired
-};
-
-PageWrapper.defaultProps = {
-  isLogin: false
+  children: PropTypes.node.isRequired
 };
 
 export default PageWrapper;
