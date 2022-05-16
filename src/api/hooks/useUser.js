5a6b5f5b673d59ec23fa1c866ca29b9b2a7fbe6d
Schatten WORK-PC <schattenwork@example.com>
2022-05-16 20:57:06 +0300

    add upload driver start-screen [TAXIT-52]

--- src/api/hooks/useUser.js
+++ src/api/hooks/useUser.js
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
 
 import { notificationTypes } from '../../shared/components/Notifications/components/Notification/notification.constants';
 import useNotifications from '../../shared/hooks/useNotifications/useNotifications';
@@ -6,9 +8,10 @@ import useAppSpinner from '../../shared/hooks/useAppSpinner';
 import { axiosService } from '../../services/axios.service';
 import { API_ROUTES } from '../../constants/api.constants';
 import { INITIAL_USER_STATE } from '../../components/client/ClientHome/client-home.constants';
-import { STORAGE_KEYS } from '../../constants/app.constants';
+import { PRIVATE_ROUTES, STORAGE_KEYS } from '../../constants/app.constants';
 
 export function useUser() {
+  const navigate = useNavigate();
   const { showSpinner, closeSpinner } = useAppSpinner();
   const { showNotification } = useNotifications();
   const [user, setUser] = useState(INITIAL_USER_STATE);
@@ -30,19 +33,24 @@ export function useUser() {
     getUser();
   }, [closeSpinner, showNotification, showSpinner, user.role]);
 
-  const uploadPhoto = async ({ id, file }) => {
-    try {
-      showSpinner();
-      await axiosService.post(`${API_ROUTES.USER}/${id}/${API_ROUTES.PHOTO}`, {
-        method: 'post',
-        body: file
-      });
-    } catch (error) {
-      showNotification(error.response.data.message, notificationTypes.error);
-    } finally {
-      closeSpinner();
-    }
-  };
+  const uploadPhoto = useCallback(
+    async ({ file }) => {
+      try {
+        const { id } = user;
+        const formData = new FormData();
+        formData.append('file', file);
+        showSpinner();
+        await axiosService.post(`${API_ROUTES.USER}/${id}/${API_ROUTES.PHOTO}`, formData);
+        showNotification('Upload successfully!', notificationTypes.success);
+        navigate(PRIVATE_ROUTES.HOME);
+      } catch (error) {
+        showNotification(error.response.data.message, notificationTypes.error);
+      } finally {
+        closeSpinner();
+      }
+    },
+    [user, showSpinner, showNotification, navigate, closeSpinner]
+  );
 
   return { user, uploadPhoto };
 }
--- src/components/driver/StartScreen/StartScreen.jsx
+++ src/components/driver/StartScreen/StartScreen.jsx
@@ -1,6 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
 
 import { useUser } from '../../../api/hooks/useUser';
+import { PRIVATE_ROUTES } from '../../../constants/app.constants';
 
 import Button from '../../../shared/components/Button/Button';
 import { buttonColors, buttonSizes } from '../../../shared/components/Button/button.constants';
@@ -10,25 +13,43 @@ import { inputType } from './start-screen.constants';
 import classes from './start-screen.module.css';
 
 function StartScreen() {
+  const navigate = useNavigate();
   const [isValid, setIsValid] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
   const fileInput = useRef();
-  const {
-    uploadPhoto,
-    user: { id }
-  } = useUser();
+  const { uploadPhoto, user } = useUser();
+
+  useEffect(() => {
+    if (user.car?.photo) {
+      return navigate(PRIVATE_ROUTES.HOME);
+    }
+    return () => false;
+  }, [navigate, user]);
+
+  useEffect(() => {
+    if (!selectedFile) {
+      return setSelectedFile(null);
+    }
+    const imageUrl = URL.createObjectURL(selectedFile);
+    setImagePreview(imageUrl);
+    setIsValid(true);
+    return () => URL.revokeObjectURL(imageUrl);
+  }, [selectedFile]);
 
   const handleFileSelected = (e) => {
     setSelectedFile(e.target.files[0]);
   };
 
-  const handleFileUpload = async () => {
+  const handleFileUpload = () => {
     fileInput.current.click();
-    const formData = new FormData();
-    formData.append('image', selectedFile);
-    await uploadPhoto({ id, formData });
-    setIsValid(true);
   };
+
+  const handleFileSave = () => {
+    uploadPhoto({ file: selectedFile });
+  };
+
+  const image = selectedFile ? imagePreview : '/public/img/wrapper_car_img.png)';
 
   return (
     <div className={classes.container}>
@@ -40,14 +61,18 @@ function StartScreen() {
           IMPORTANT: You can upload photo only once!
         </p>
         <div className={classes.photo__content}>
-          <div className={classes.photo} />
+          <div
+            className={classes.photo}
+            style={{
+              backgroundImage: `url(${image})`
+            }}
+          />
           <div className={classes.block__buttons}>
             <input
               className={classes.input_file}
               type={inputType.file}
               onChange={handleFileSelected}
-              name="img"
-              accept="image/png, image/jpeg"
+              accept=".png, .jpeg, .jpg"
               ref={fileInput}
             />
             <Button size={buttonSizes.big} color={buttonColors.general} onClick={handleFileUpload}>
@@ -56,7 +81,7 @@ function StartScreen() {
             <Button
               size={buttonSizes.big}
               color={buttonColors.accept}
-              onClick={handleFileUpload}
+              onClick={handleFileSave}
               className={classes.button}
               disabled={!isValid}>
               Save
