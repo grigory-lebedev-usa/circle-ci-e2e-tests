eff663073e007d758c38ca4b98115b5eaf3e0920
Schatten <schattenoff@example.com>
2022-07-04 18:14:14 +0300

    add function registerInterceptors [TAXIT-90]

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -7,37 +7,39 @@ import LocalStorageService from './LocalStorageService';
 
 export const axiosService = axios.create({ baseURL: APP_CONFIG.apiURL });
 
-axiosService.interceptors.request.use((conf) => {
-  const config = conf;
-  const { accessToken } = LocalStorageService;
-  config.headers.Authorization = `Bearer ${accessToken}`;
-  return config;
-});
-
-axiosService.interceptors.response.use(
-  (config) => {
+export function registerInterceptors() {
+  axiosService.interceptors.request.use((conf) => {
+    const config = conf;
+    const { accessToken } = LocalStorageService;
+    config.headers.Authorization = `Bearer ${accessToken}`;
     return config;
-  },
-  // eslint-disable-next-line func-names
-  async function (error) {
-    const storedRequest = error.config;
-    const { keepUserLoginIn, refreshToken: originalRefreshToken } = LocalStorageService;
-    // eslint-disable-next-line no-underscore-dangle
-    if (error.response.status === 401 && keepUserLoginIn && !storedRequest._retry) {
+  });
+
+  axiosService.interceptors.response.use(
+    (config) => {
+      return config;
+    },
+    // eslint-disable-next-line func-names
+    async function (error) {
+      const storedRequest = error.config;
+      const { keepUserLoginIn, refreshToken: originalRefreshToken } = LocalStorageService;
       // eslint-disable-next-line no-underscore-dangle
-      storedRequest._retry = true;
-      const {
-        data: { accessToken, refreshToken, expirationTime }
-      } = await axiosService.post(API_ROUTES.REFRESH, {
-        refreshToken: originalRefreshToken
-      });
-      LocalStorageService.accessToken = accessToken;
-      LocalStorageService.refreshToken = refreshToken;
-      LocalStorageService.expirationTime = expirationTime;
-      axiosService.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-      return axiosService(storedRequest);
+      if (error.response.status === 401 && keepUserLoginIn && !storedRequest._retry) {
+        // eslint-disable-next-line no-underscore-dangle
+        storedRequest._retry = true;
+        const {
+          data: { accessToken, refreshToken, expirationTime }
+        } = await axiosService.post(API_ROUTES.REFRESH, {
+          refreshToken: originalRefreshToken
+        });
+        LocalStorageService.accessToken = accessToken;
+        LocalStorageService.refreshToken = refreshToken;
+        LocalStorageService.expirationTime = expirationTime;
+        axiosService.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+        return axiosService(storedRequest);
+      }
+      LocalStorageService.clear();
+      return Promise.reject(error);
     }
-    LocalStorageService.clear();
-    return Promise.reject(error);
-  }
-);
+  );
+}
