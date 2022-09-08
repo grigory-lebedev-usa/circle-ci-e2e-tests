a39e7b219b348588205914ef93f49c2da6eea410
grigory-lebedev <hryhory.lebedzev@rockspoon.com>
2022-09-08 18:45:39 +0300

    1

--- __checks__/auth.check.js
+++ __checks__/auth.check.js
@@ -1,27 +1,13 @@
-const {test} = require('@playwright/test');
+const {chromium} = require('@playwright/test');
 
-test.use({
-  headless: false,
-  baseURL: 'https://tomas-taxi.vercel.app'
-});
+const run = async () => {
+  const browser = await chromium.launch({ headless: false })
+  const page = await browser.newPage();
+  await page.goto('https://tomas-taxi.vercel.app/login');
+  await page.locator('#email').fill('lebedevgrisha21@gmail.com');
+  await page.locator('#password').fill('4815162342l');
+  await page.locator('text="Login"').click();
+  await page.waitForNavigation({url: '/home'});
+};
 
-test.describe.serial("Authentication", () => {
-
-  let page;
-
-  test.beforeAll(async ({browser}) => {
-    page = await browser.newPage();
-  });
-
-  test("Login", async () => {    
-    await page.goto('/login');
-    await page.locator('#email').fill('lebedevgrisha21@gmail.com');
-    await page.locator('#password').fill('4815162342l');
-    await page.locator('text="Login"').click();
-    await page.waitForNavigation({url: '/home'});
-  });
-
-  test.afterAll(async () => {
-      await page.close();
-  });
-});
+run();
