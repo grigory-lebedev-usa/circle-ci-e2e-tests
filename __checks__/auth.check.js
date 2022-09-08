7eb36e1bfd3e547314ccf7773888e7b7173e4fdb
grigory-lebedev <hryhory.lebedzev@rockspoon.com>
2022-09-08 19:08:52 +0300

    1

--- __checks__/auth.check.js
+++ __checks__/auth.check.js
@@ -1,13 +1,17 @@
-const {chromium} = require('@playwright/test');
+const { chromium } = require('playwright')
 
-const run = async () => {
+async function run () {
   const browser = await chromium.launch();
   const page = await browser.newPage();
+
   await page.goto('https://tomas-taxi.vercel.app/login');
   await page.locator('#email').fill('lebedevgrisha21@gmail.com');
   await page.locator('#password').fill('4815162342l');
   await page.locator('text="Login"').click();
-  await page.waitForNavigation({url: '/home'});
-};
+  await page.waitForNavigation({ url: 'https://tomas-taxi.vercel.app/home' });
+
+  await page.close();
+  await browser.close();
+}
 
 run();
